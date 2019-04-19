96ff985209b3fb2487ea1ac9697078edb8314926
HUNTER\EMagallanes <elijah.magallanes@hunterindustries.com>
2019-04-19 16:35:27 -0700

    Added functionality to build the table for the website;

--- statisticsController.js
+++ statisticsController.js
@@ -1,8 +1,31 @@
 'use strict';
 
-function buildStatistics(stats) {
-    console.log(stats);
+function buildStatistics(stats, statsHeader, statsBody, document) {
+    const headerRow = document.createElement('tr');
+    Object.keys(stats[0]).forEach(statistic => {
+        const headerElement = document.createElement('th');
+        const headerElementText = document.createTextNode(statistic);
+        headerElement.appendChild(headerElementText);
+        headerRow.appendChild(headerElement);
+    });
+
+    statsHeader.appendChild(headerRow);
+
+    for (let i = 0; i < stats.length; i++) {
+        const curPlayer = stats[i];
+        const playerRow = document.createElement('tr');
+
+        Object.keys(curPlayer).forEach(statistic => {
+            const statElement = document.createElement('td');
+            const statElementText = document.createTextNode(curPlayer[statistic]);
+            statElement.appendChild(statElementText);
+            playerRow.appendChild(statElement);
+        });
+
+        statsBody.appendChild(playerRow);
+    }
 }
+
 
 module.exports = {
     buildStatistics
